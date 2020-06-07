32d383fe6a234f97f318c95907a1d10912f1c32f
Nikkisuu <61058001+nikkitsuu@users.noreply.github.com>
2020-06-06 20:24:32 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,12 +1,5 @@
 #! /usr/bin/env python3
 
-"""
-Sherlock: Find Usernames Across Social Networks Module
-
-This module contains the main logic to search for usernames at social
-networks.
-"""
-
 import sherlock
 
 
